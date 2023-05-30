3834c1aefd4d6118803eb971a57af3044e9b4f48
Marcelo Ferreira Mota Júnior <marcelo.mota.088@ufrn.edu.br>
2023-05-30 20:18:17 -0300

    @mfmjr

--- 03 - js/01 - dark mode/01 - initial/script.js
+++ 03 - js/01 - dark mode/01 - initial/script.js
@@ -1,10 +1,4 @@
 const container = document.querySelector(".container");
 const btnDarkMode = document.querySelector(".toggle");
 
-const btnDarkModeClicked = () => {
-  container.classList.toggle("darkMode")
-    ? (btnDarkMode.firstElementChild.className = "far fa-moon")
-    : (btnDarkMode.firstElementChild.className = "far fa-sun");
-};
-
-btnDarkMode.addEventListener("click", btnDarkModeClicked);
+ 
