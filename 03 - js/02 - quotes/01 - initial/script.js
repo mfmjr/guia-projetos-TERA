d0dc0ae1df7dda2201711dfedcfc0c16a1171d51
Marcelo Ferreira Mota Júnior <marcelo.mota.088@ufrn.edu.br>
2023-06-03 06:46:48 -0300

    Origamid

--- 03 - js/02 - quotes/01 - initial/script.js
+++ 03 - js/02 - quotes/01 - initial/script.js
@@ -3,16 +3,8 @@ const btnQuote = document.querySelector(".btnQuote");
 const author = document.querySelector(".author");
 
 const btnGetJokeClicked = async () => {
-  try {
-    const url = "https://type.fit/api/quotes";
-    const response = await fetch(url);
-    const data = await response.json();
-    const randomJoke = Math.floor(Math.random() * data.length);
-    textquote.textContent = `"${data[randomJoke].text}"`;
-    author.textContent = `${data[randomJoke].author}`;
-  } catch (error) {
-    throw new Error(error);
-  }
+    const url = "https://type.fit/api/quotes";   
+
 };
 
-btnQuote.addEventListener("click", btnGetJokeClicked);
+
