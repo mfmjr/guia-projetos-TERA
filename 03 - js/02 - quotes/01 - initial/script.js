af58f593d4d28de6d09bff845267a27449dbe9a6
Marcelo Ferreira Mota Júnior <marcelo.mota.088@ufrn.edu.br>
2023-06-07 19:44:54 -0300

    @mfmjr

---
+++ 03 - js/02 - quotes/01 - initial/script.js
@@ -0,0 +1,10 @@
+const textquote = document.querySelector(".quote");
+const btnQuote = document.querySelector(".btnQuote");
+const author = document.querySelector(".author");
+
+const btnGetJokeClicked = async () => {
+    const url = "https://type.fit/api/quotes";   
+
+};
+
+
