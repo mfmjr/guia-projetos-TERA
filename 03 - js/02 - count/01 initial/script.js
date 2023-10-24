1e02f91216403ab7cd774d0f66950bb3571444e8
Marcelo Ferreira Mota Júnior <marcelo.mota.088@ufrn.edu.br>
2023-10-23 23:16:59 -0300

    somostera

--- 03 - js/02 - count/01 initial/script.js
+++ 03 - js/02 - count/01 initial/script.js
@@ -2,3 +2,19 @@ const btnIncrement = document.querySelector(".increment");
 const btnDecrement = document.querySelector(".decrement");
 const btnClear = document.querySelector(".clear");
 let value = document.querySelector("#value");
+
+const btnIncrementClicked = () => {
+  value.textContent = Number(value.textContent) + 1;
+};
+
+const btnDecrementClicked = () => {
+  value.textContent = Number(value.textContent) - 1;
+};
+
+const btnClearClicked = () => {
+  value.textContent = Number(0);
+};
+
+btnIncrement.addEventListener("click", btnIncrementClicked);
+btnDecrement.addEventListener("click", btnDecrementClicked);
+btnClear.addEventListener("click", btnClearClicked);
